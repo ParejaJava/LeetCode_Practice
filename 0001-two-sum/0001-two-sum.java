a9f6db470856859be48a0f30be7f87f38d7f614f
ParejaJava <spj1336979241@gmail.com>
2026-02-02 21:39:03 +0800

    2 ms (99.14%) 46.9 MB (74.96%) 02-02-2026 0001-two-sum Easy .java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                if (j == i){
-                    continue;
-                }
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> table = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(table.containsKey(target - nums[i])){
+                return new int[]{i,table.get(target - nums[i])};
+            }
+            else{
+                table.put(nums[i], i);
             }
         }
-    return new int[]{};
+        return new int[]{};
     }
 }
