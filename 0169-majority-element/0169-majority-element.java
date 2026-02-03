ba8e07321a797f51d0e199ccc2967abbc2516b60
ParejaJava <spj1336979241@gmail.com>
2026-02-03 22:14:55 +0800

    2 ms (71.57%) 55.8 MB (27.49%) 02-03-2026 0169-majority-element Easy .java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,32 @@
-class Solution {
-    public int majorityElement(int[] nums) { // 排序法
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+class Solution { // 分治法
+    private int countForConflict(int[] nums, int num, int lo , int hi){
+        int count = 0;
+        for (int i = lo; i <= hi; i++){ // 一定注意这里需要是小于等于
+            if (num == nums[i]){
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    private int divideAndConquer(int lo, int hi, int[] nums){
+        if (lo == hi){
+            return nums[lo];
+        }
+        int mid = lo + (hi - lo)/2;
+
+        int leftResult = divideAndConquer(lo, mid, nums);
+        int rightResult = divideAndConquer(mid + 1, hi, nums);
+
+        if (leftResult == rightResult){
+            return leftResult;
+        }else{
+            int leftcount = countForConflict(nums, leftResult, lo, hi);
+            int rightcount = countForConflict(nums, rightResult, lo, hi);
+            return leftcount > rightcount ? leftResult : rightResult;
+        }
+    }
+    public int majorityElement(int[] nums) { 
+        return divideAndConquer(0, nums.length - 1, nums);
     }
 }
