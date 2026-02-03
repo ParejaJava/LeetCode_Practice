4d39102a00724b0a0140d1587755175570a31c86
ParejaJava <spj1336979241@gmail.com>
2026-02-03 21:12:14 +0800

    7 ms (43.33%) 56 MB (14.56%) 02-03-2026 0169-majority-element Easy .java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> table = new HashMap<>();//哈希表法（自己写的）
-        for (int num : nums){
-            table.put(num, table.getOrDefault(num, 0) + 1); //Gemini推荐的录数组新API
-        }
-        int maxtimes = 0;
-        int most_num = 0;
-        for (Map.Entry <Integer, Integer> entry : table.entrySet()){
-            if (entry.getValue() > maxtimes){
-                maxtimes = entry.getValue();
-                most_num = entry.getKey();
-            }
-
-        }
-        return most_num;
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
