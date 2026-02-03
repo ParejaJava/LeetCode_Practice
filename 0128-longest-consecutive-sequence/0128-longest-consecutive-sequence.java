1c0d9afd1be04698c249e06ceb03026bc20d3cc0
ParejaJava <spj1336979241@gmail.com>
2026-02-03 19:38:43 +0800

    Update 0128-longest-consecutive-sequence.java

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,11 +1,11 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(); //先把数组存入哈希set去重
         for (int num : nums){
             set.add(num);
         }
         int score = 0;
-        for (int num : set){
+        for (int num : set){ //这里记得用新哈希表遍历
             if (!set.contains(num - 1)){
                 int curr_num = num;
                 int curr_length = 1;
