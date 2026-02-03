631894f23da1596cec6e7adced0e38f9b15e6a52
ParejaJava <spj1336979241@gmail.com>
2026-02-03 20:57:33 +0800

    17 ms (15.63%) 52.6 MB (89.16%) 02-03-2026 0169-majority-element Easy .java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -24,8 +24,3 @@ class Solution {
         return majorityEntry.getKey();
     }
 }
-
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/majority-element/solutions/146074/duo-shu-yuan-su-by-leetcode-solution/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
