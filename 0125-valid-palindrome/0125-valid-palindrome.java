be7f30513d6ffd4bf42cb2ed65fc207724662382
ParejaJava <spj1336979241@gmail.com>
2026-02-05 20:20:08 +0800

    4 ms (51.44%) 44.7 MB (51.09%) 02-05-2026 0125-valid-palindrome Easy .java

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,26 @@
 class Solution {
-    public boolean isPalindrome(String s) { // API版
+    public boolean isPalindrome(String s) { // 双指针版
         StringBuilder sb = new StringBuilder();
-        for (char ch : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
             if (Character.isLetterOrDigit(ch)) {
                 sb.append(Character.toLowerCase(ch));
             }
         }
-        String ssr = sb.toString();
-        String sr = sb.reverse().toString();
-        return sr.equals(ssr);
+        String ssr = sb.toString(); 
+        int L = 0;
+        int R = ssr.length() - 1;
+        
+        while(L < R) {
+            char ch1 = ssr.charAt(L);
+            char ch2 = ssr.charAt(R);
+            if (ch1 != ch2) {
+                return false;
+            } else {
+                L++;
+                R--;
+            }
+        }
+        return true;
     }
 }
