91a48541b41268da128a8abce3d1a51391f1555a
ParejaJava <spj1336979241@gmail.com>
2026-02-15 00:23:19 +0800

    N/A (0%) N/A (0%) 02-15-2026 0977-squares-of-a-sorted-array Easy .java

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -2,10 +2,48 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
         int[] numSquare = new int[n];
-        for (int i = 0; i < n; i++) {
-            numSquare[i] = nums[i] * nums[i];
+        int i = 0;
+        while (i < n && nums[i] < 0) {
+            i++;
         }
-        Arrays.sort(numSquare);
-        return numSquare;
+        if (i == 0) {
+            for (int a = 0; a < n; a++) {
+                numSquare[a] = nums[a] * nums[a];
+            }
+            return numSquare;
+        }else if (i == n) {
+            int j = n - 1; // 初始下标别越界
+            for (int b = 0; b < n; b++) {
+                numSquare[b] = nums[j] * nums[j];
+                j--;
+            }
+            return numSquare;
+        }else {
+            int neg = i - 1;
+            int p = i; // p == pos
+            int idx = 0;
+            while (neg >= 0 || p < n) {
+                if (neg < 0) {
+                    numSquare[idx] = nums[p] * nums[p];
+                    idx++; //记得维护所有指针
+                    continue; //忘了
+                }
+                if (p > n - 1) {
+                    numSquare[idx] = nums[neg] * nums[neg];
+                    idx++;
+                    continue; //忘了
+                }
+                if (nums[neg] * nums[neg] < nums[p] * nums[p]) {
+                    numSquare[idx] = nums[neg] * nums[neg];
+                    idx++;
+                    neg--;
+                }else {
+                    numSquare[idx] = nums[p] * nums[p];
+                    idx++;
+                    p++;
+                }
+            }
+            return numSquare;
+        }
     }
 }
