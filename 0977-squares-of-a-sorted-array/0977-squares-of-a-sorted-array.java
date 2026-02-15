d586f5222dae87206884db4aaa611a7568300740
ParejaJava <spj1336979241@gmail.com>
2026-02-15 11:26:22 +0800

    1 ms (99.81%) 47.4 MB (52%) 02-15-2026 0977-squares-of-a-sorted-array Easy .java

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -6,46 +6,32 @@ class Solution {
         while (i < n && nums[i] < 0) {
             i++;
         }
-        if (i == 0) {
-            for (int a = 0; a < n; a++) {
-                numSquare[a] = nums[a] * nums[a];
+        int neg = i - 1;
+        int p = i; // p == pos
+        int idx = 0;
+        while (neg >= 0 || p < n) {
+            if (neg < 0) {
+                numSquare[idx] = nums[p] * nums[p];
+                p++;
+                idx++; //记得维护所有指针
+                continue; //忘了
             }
-            return numSquare;
-        }else if (i == n) {
-            int j = n - 1; // 初始下标别越界
-            for (int b = 0; b < n; b++) {
-                numSquare[b] = nums[j] * nums[j];
-                j--;
+            if (p > n - 1) {
+                numSquare[idx] = nums[neg] * nums[neg];
+                neg--;
+                idx++;
+                continue; //忘了
             }
-            return numSquare;
-        }else {
-            int neg = i - 1;
-            int p = i; // p == pos
-            int idx = 0;
-            while (neg >= 0 || p < n) {
-                if (neg < 0) {
-                    numSquare[idx] = nums[p] * nums[p];
-                    p++;
-                    idx++; //记得维护所有指针
-                    continue; //忘了
-                }
-                if (p > n - 1) {
-                    numSquare[idx] = nums[neg] * nums[neg];
-                    neg--;
-                    idx++;
-                    continue; //忘了
-                }
-                if (nums[neg] * nums[neg] < nums[p] * nums[p]) {
-                    numSquare[idx] = nums[neg] * nums[neg];
-                    idx++;
-                    neg--;
-                }else {
-                    numSquare[idx] = nums[p] * nums[p];
-                    idx++;
-                    p++;
-                }
+            if (nums[neg] * nums[neg] < nums[p] * nums[p]) {
+                numSquare[idx] = nums[neg] * nums[neg];
+                idx++;
+                neg--;
+            }else {
+                numSquare[idx] = nums[p] * nums[p];
+                idx++;
+                p++;
             }
-            return numSquare;
         }
+        return numSquare;
     }
 }
