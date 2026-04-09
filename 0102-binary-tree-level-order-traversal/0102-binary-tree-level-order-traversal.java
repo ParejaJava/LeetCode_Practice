3023ea43e11300c7e71a292157acb15989182599
ParejaJava <spj1336979241@gmail.com>
2026-04-09 22:29:08 +0800

    1 ms (95.2%) 46.8 MB (68.46%) 04-09-2026 0102-binary-tree-level-order-traversal Medium .java

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> que = new ArrayDeque<>();
+        que.offer(root);
+
+        while(!que.isEmpty()) {
+            List<Integer> levelres = new ArrayList<>();
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                levelres.add(node.val);
+                if(node.left != null) {
+                    que.offer(node.left);
+                }
+                if(node.right != null) {
+                    que.offer(node.right);
+                }
+            }
+            res.add(levelres);
+        }
+        return res;
+    }
+}
