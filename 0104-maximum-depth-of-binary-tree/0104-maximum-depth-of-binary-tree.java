4d185746bb7b57e39154448b2bf1d66ae50adc44
ParejaJava <spj1336979241@gmail.com>
2026-04-09 23:18:56 +0800

    1 ms (11.53%) 44.4 MB (54.28%) 04-09-2026 0104-maximum-depth-of-binary-tree Easy .java

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> que = new ArrayDeque<>();
+        que.offer(root);
+        int depth = 0;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                if(node.left != null) {
+                    que.offer(node.left);
+                }
+                if(node.right != null) {
+                    que.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
