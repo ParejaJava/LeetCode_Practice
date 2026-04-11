8a9ab0e3bf64cb13659e19963a2ffe80ef38bc01
ParejaJava <spj1336979241@gmail.com>
2026-04-11 20:27:13 +0800

    0 ms (100%) 49.3 MB (73.46%) 04-11-2026 0222-count-complete-tree-nodes Easy .java

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -18,6 +18,21 @@ class Solution {
         if (root == null) {
             return 0;
         }
+        TreeNode l = root.left;
+        TreeNode r = root.right;
+        int hl = 1;
+        int hr = 1;
+        while (l != null) {
+            l = l.left;
+            hl++;
+        }
+        while (r != null) {
+            r = r.right;
+            hr++;
+        }
+        if (hl == hr) {
+            return (int)Math.pow(2, hl) - 1;
+        } 
         return countNodes(root.left) + countNodes(root.right) + 1;
     }
 }
