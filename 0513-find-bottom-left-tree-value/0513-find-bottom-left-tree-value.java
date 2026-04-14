5b87426a95295bc6e2ae05cd3ff5b302e4b6a10d
ParejaJava <spj1336979241@gmail.com>
2026-04-14 20:21:35 +0800

    4 ms (14.64%) 46.4 MB (66.67%) 04-14-2026 0513-find-bottom-left-tree-value Medium .java

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> que = new ArrayDeque<>();
+        Queue<TreeNode> qtmp = new ArrayDeque<>();
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                if (i == 0) {
+                    qtmp.offer(node);
+                }
+                if (node.left != null) {
+                    que.offer(node.left);
+                }
+                if (node.right != null) {
+                    que.offer(node.right);
+                }
+            }
+            if (que.isEmpty()) {
+                return qtmp.poll().val;
+            }
+            else {
+                qtmp.poll();
+            }
+        }
+        return 0;
+    }
+}
