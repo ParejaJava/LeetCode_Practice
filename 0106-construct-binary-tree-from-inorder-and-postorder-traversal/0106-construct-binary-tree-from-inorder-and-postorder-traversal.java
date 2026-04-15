30f39098a254a691d455c35838d6952bf5463927
ParejaJava <spj1336979241@gmail.com>
2026-04-15 23:41:09 +0800

    2 ms (63.8%) 46.3 MB (52.89%) 04-15-2026 0106-construct-binary-tree-from-inorder-and-postorder-traversal Medium .java

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    // 这一版是左闭右开写法，以后都坚持这种写法
     Map<Integer, Integer> inorderMap = new HashMap<>();
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if (inorder.length == 0 || postorder.length == 0) {
@@ -22,19 +23,19 @@ class Solution {
         for (int i = 0; i < inorder.length; i++) {
             inorderMap.put(inorder[i], i);
         }
-        return cut(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
+        return cut(inorder, 0, inorder.length, postorder, 0, postorder.length);
     }
 
     private TreeNode cut(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
-        if (inStart > inEnd || postStart > postEnd) {
+        if (inStart >= inEnd || postStart >= postEnd) {
             return null;
         }
-        int rootVal = postorder[postEnd];
+        int rootVal = postorder[postEnd-1];
         TreeNode root = new TreeNode(rootVal);
         int rootIndex = inorderMap.get(rootVal);
         int leftSize = rootIndex - inStart;
-        root.left = cut(inorder, inStart, rootIndex-1, 
-                    postorder, postStart, postStart+leftSize-1);
+        root.left = cut(inorder, inStart, rootIndex, 
+                    postorder, postStart, postStart+leftSize);
         root.right = cut(inorder, rootIndex+1, inEnd, 
                     postorder, postStart+leftSize, postEnd-1);
         return root;
