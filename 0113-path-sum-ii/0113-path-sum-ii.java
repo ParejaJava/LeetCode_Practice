1b6e3573faa76605e1b3646a52f8a0d059928379
ParejaJava <spj1336979241@gmail.com>
2026-04-15 20:27:29 +0800

    1 ms (100%) 45.8 MB (15.38%) 04-15-2026 0113-path-sum-ii Medium .java

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -25,24 +25,17 @@ class Solution {
     }
 
     private void traversal(TreeNode root, int targetSum, List<List<Integer>> res, 
-    List<Integer> paths) {
+    List<Integer> paths) { // 严肃学习这段代码，进来第一件事加，出去最后一件事删，完美的递归回溯
         paths.add(root.val);  
         if (root.left == null && root.right == null) {
             if (root.val == targetSum) {
                 res.add(new ArrayList<>(paths));
-                return;
-            }
-            else {
-                return;
             }
         }
-        if (root.left != null){
-            traversal(root.left, targetSum - root.val, res, paths);
-            paths.remove(paths.size()-1);
+        else { //这个else是灵魂，因为在排除叶子节点的寻找过程中，不需要做paths的回退操作
+        if (root.left != null) traversal(root.left, targetSum - root.val, res, paths);
+        if (root.right != null) traversal(root.right, targetSum - root.val, res, paths);
         }
-        if (root.right != null) {
-            traversal(root.right, targetSum - root.val, res, paths);
-            paths.remove(paths.size()-1);
-        }
+        paths.remove(paths.size()-1);
     }
 }
