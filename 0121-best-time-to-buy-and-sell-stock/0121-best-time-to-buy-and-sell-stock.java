c1089e51f23553be4c20110e2e7b19eb8e9d570a
ParejaJava <spj1336979241@gmail.com>
2026-05-22 23:05:44 +0800

    1 ms (99.93%) 94.5 MB (31.59%) 05-22-2026 0121-best-time-to-buy-and-sell-stock Easy .java

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int minPrice = prices[0];
+        for(int p : prices) {
+            ans = Math.max(ans, p - minPrice);
+            minPrice = Math.min(minPrice, p);
+        }
+        return ans;
+    }
+}
