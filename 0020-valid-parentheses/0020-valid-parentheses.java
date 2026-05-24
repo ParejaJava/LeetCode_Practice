1a3287fcb4e251277861825fc487f810a5200e34
ParejaJava <spj1336979241@gmail.com>
2026-05-24 18:01:47 +0800

    4 ms (39.69%) 43.6 MB (15.26%) 05-24-2026 0020-valid-parentheses Easy .java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        int n = s.length();
-        if (n % 2 != 0) {
-            return false;
-        }
-        Deque<Character> stack = new ArrayDeque<>();
-        Map<Character, Character> pairs = Map.of(
-            ')', '(',
-            ']', '[',
-            '}', '{');
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            if (pairs.containsKey(ch)) {
-                if (stack.peek() != pairs.get(ch)) {
-                    return false;
-                }
-                stack.pop();            
-            }
-            else {
-                stack.push(ch);
+        Map<Character, Character> map = new HashMap<>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> st = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if(!map.containsKey(c)) { // 是左括号
+                st.push(c);
+            } else if (st.isEmpty() || st.pop() != map.get(c)) {
+                return false;
             }
         }
-        return stack.isEmpty();
+
+        return st.isEmpty();
     }
 }
