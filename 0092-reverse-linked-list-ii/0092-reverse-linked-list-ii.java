97365413bbe08cbeebf08fed33e9c7d6580a62b0
ParejaJava <spj1336979241@gmail.com>
2026-05-25 13:19:47 +0800

    0 ms (100%) 42.7 MB (95.48%) 05-25-2026 0092-reverse-linked-list-ii Medium .java

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,21 +10,21 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode p0 = dummy;
-        for (int i = 0; i < left-1; i++) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode p0 = dummy; // p0最终指向的应该是left左边的相邻节点
+        int l = left - 1;
+        while (l > 0) {
             p0 = p0.next;
+            l--;
         }
-        ListNode prev = null;
+        ListNode prev = p0;
         ListNode curr = p0.next;
-        ListNode nxt = null;
-        for (int i = 0; i < right-left+1; i++) {
-            nxt = curr.next;
+        for (int i = 0; i < right - left + 1; i++) {
+            ListNode nxt = curr.next;
             curr.next = prev;
             prev = curr;
             curr = nxt;
         }
-
         p0.next.next = curr;
         p0.next = prev;
         return dummy.next;
