a3beb89a6597f97909b77aa10828142eca0c516c
ParejaJava <spj1336979241@gmail.com>
2026-05-25 23:30:38 +0800

    46 ms (63.68%) 130.8 MB (82.72%) 05-25-2026 0146-lru-cache Medium .java

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,31 +1,65 @@
 class LRUCache {
-    private final int capacity;
-    private final Map<Integer, Integer> map = new LinkedHashMap<>();
+    private static class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private int capacity;
+    private Node dummy = new Node(0, 0);
+    private Map<Integer, Node> keyToNode = new HashMap<>();
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        dummy.prev = dummy;
+        dummy.next = dummy;
     }
     
-    public int get(int key) {
-        Integer value = map.remove(key); // remove返回删除值或者空，所以必须用包装类Integer
-        if (value != null) {
-            map.put(key, value);
-            return value;
-        }
-        return -1;
+    public int get(int key) { // 这个函数目的是查看key对应的value并且把这对kv放到链表头
+        Node node = getNode(key);
+        return node != null ? node.value : -1;
     }
     
-    public void put(int key, int value) {
-        Integer v = map.remove(key);
-        if (v != null) {
-            map.put(key, value);
-        } else {
-            if (map.size() == capacity) {
-                map.remove(map.keySet().iterator().next());
-            }
-            map.put(key, value);
+    public void put(int key, int value) { // 这个函数的目的是：如果有这个key更新值并提前即可
+    // 没有这个key的话需要新建一个节点然后放在头部
+        Node node = getNode(key);// 此时已经移动到头部
+        if (node != null) {
+            node.value = value;// 修改为新值
+            return;
         }
+        node = new Node(key, value); // 没有这个节点要新建
+        keyToNode.put(key, node); // 同时也要存入哈希表，因为需要o1查找
+        pushFront(node); // 放到链表头
+        if (keyToNode.size() > capacity) {// 看看有没有超过容限
+            Node backNode = dummy.prev;
+            keyToNode.remove(backNode.key);
+            remove(backNode); // 找到尾部节点删掉，在字典里也一并删掉
+        }
+    }
 
+    private Node getNode(int key) { //没key就返回空，有key要“抽出”然后“放到头部”
+        if (!keyToNode.containsKey(key)) {
+            return null;
+        }
+        Node node = keyToNode.get(key);
+        remove(node);
+        pushFront(node);
+        return node;
+    }
+
+    private void remove (Node x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+    }
+
+    private void pushFront (Node x) {
+        x.prev = dummy;
+        x.next = dummy.next;
+        x.prev.next = x;
+        x.next.prev = x;
     }
 }
 
