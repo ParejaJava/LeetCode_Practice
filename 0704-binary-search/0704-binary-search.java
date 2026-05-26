afbd9ceffabf64d5ce9bc426219c6993b278dc46
ParejaJava <spj1336979241@gmail.com>
2026-05-26 17:01:13 +0800

    0 ms (100%) 48.5 MB (30.65%) 05-26-2026 0704-binary-search Easy .java

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right -left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (left == nums.length || nums[left] != target) return -1;
+        return left;
+    }
+}
